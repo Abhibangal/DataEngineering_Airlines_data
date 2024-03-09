48551c2a7b56f50c474ddc96a4261ebc530b27c3
abhij <abhijit.bangal92@gmail.com>
2024-03-09 11:53:32 +0530

    deleted some file

--- airflow/dags/credential.py
+++
@@ -1,6 +0,0 @@
-import keyring as kring
-
-kring.set_password('OTT_Details_API','api_key','c29ca49425msheaa52ccaa3c97d1p1b0376jsn2fe766fafc0e')
-kring.set_password('OTT_Details_API','host_name','moviesminidatabase.p.rapidapi.com')
-
-print(kring.get_password('OTT_Details_API','host_name'))
